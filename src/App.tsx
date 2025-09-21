711dd69b0d8d50752b34123e48f78a7907c7c6a2
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2025-09-21 02:35:17 -0500

    feat: remove <Analytics /> component from the App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import AppRouter from "./AppRouter";
 import { AuthProvider } from "./context/AuthContext";
 import { MoviesProvider } from "./context/MoviesContext";
 import { ThemeProvider } from "./context/ThemeContext";
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
     <>
       <ThemeProvider>
-        <Analytics />
         <AuthProvider>
           <MoviesProvider>
             <AppRouter />
